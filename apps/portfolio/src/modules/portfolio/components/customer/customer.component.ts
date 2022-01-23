d9770b8ef7d69f1c93c4a26454260251ac8631bd
Benjamin Montazer <benjamin.montazer@protonmail.com>
2022-01-23 15:59:11 +0100

    refactor: extract customer component from portfolio

---
+++ apps/portfolio/src/modules/portfolio/components/customer/customer.component.ts
@@ -0,0 +1,13 @@
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Partner } from '../../models/partner.model';
+
+@Component({
+  selector: 'portfolio-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CustomerComponent {
+  @Input()
+  public customer!: Partner;
+}
--- apps/portfolio/src/modules/portfolio/portfolio.module.ts
+++ apps/portfolio/src/modules/portfolio/portfolio.module.ts
@@ -4,10 +4,12 @@ import { PortfolioSection } from './portfolio.section';
 import { SvgIconsModule } from '@ngneat/svg-icon';
 import { brandsIcons } from '@icons/brands';
 import { PARTNERS_PROVIDER } from './providers/partners.provider';
+import { CardModule } from 'primeng/card';
+import { CustomerComponent } from './components/customer/customer.component';
 
 @NgModule({
-  declarations: [PortfolioSection],
-  imports: [CommonModule, SvgIconsModule.forChild(brandsIcons)],
+  declarations: [PortfolioSection, CustomerComponent],
+  imports: [CommonModule, CardModule, SvgIconsModule.forChild(brandsIcons)],
   providers: [PARTNERS_PROVIDER],
   exports: [PortfolioSection],
 })

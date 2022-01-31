cfda1c7594dc3da9375448b40d7ec82694c51c4b
Benjamin Montazer <benjamin.montazer@protonmail.com>
2022-02-01 00:14:44 +0100

    fix: responsive margins

--- apps/portfolio/src/modules/contact/contact.module.ts
+++ apps/portfolio/src/modules/contact/contact.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { contactsIcons } from '@icons/contacts';
 import { SvgIconsModule } from '@ngneat/svg-icon';
+import { AnimationModule } from '@utils/animation';
 import { ButtonModule } from 'primeng/button';
 import { TooltipModule } from 'primeng/tooltip';
 
@@ -14,6 +15,7 @@ import { CONTACTS_PROVIDER } from './providers/contacts.provider';
     CommonModule,
     ButtonModule,
     TooltipModule,
+    AnimationModule,
     SvgIconsModule.forChild(contactsIcons),
   ],
   providers: [CONTACTS_PROVIDER],

a467300efb7fd1c01305ac8bcb285f36260beade
Benjamin Montazer <benjamin@montazer.dev>
2022-11-27 13:43:55 +0100

    chore: update nx

--- apps/portfolio/jest.config.ts
+++ apps/portfolio/jest.config.ts
@@ -1,5 +1,6 @@
-module.exports = {
-  displayName: 'utils',
+/* eslint-disable */
+export default {
+  displayName: 'portfolio',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +9,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/libs/utils',
+  coverageDirectory: '../../coverage/apps/portfolio',
   transform: {
     '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
--- libs/utils/jest.config.ts
+++ libs/utils/jest.config.ts
@@ -1,5 +1,6 @@
-module.exports = {
-  displayName: 'portfolio',
+/* eslint-disable */
+export default {
+  displayName: 'utils',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +9,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/portfolio',
+  coverageDirectory: '../../coverage/libs/utils',
   transform: {
     '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
   },

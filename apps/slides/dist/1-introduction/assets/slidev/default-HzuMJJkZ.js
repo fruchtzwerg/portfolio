d02ee7a0e5f232cb7312aeee550beba835a429f4
Benjamin Montazer <benjamin@montazer.dev>
2025-05-15 13:59:02 +0200

    fix(slides): add new exam api-spec url

---
+++ apps/slides/dist/1-introduction/assets/slidev/default-HzuMJJkZ.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-Ba9hXoo6.js";import{_ as r}from"../index-iVm7HSWI.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};

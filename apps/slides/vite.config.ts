a52e5a2f558cd814845130314584b43a35984695
Benjamin Montazer <benjamin@montazer.dev>
2026-06-07 14:07:30 +0200

    chore(slides): update deps

--- apps/slides/vite.config.ts
+++ apps/slides/vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 
+import Icons from 'unplugin-icons/vite';
+
 export default defineConfig({
+  plugins: [Icons({ compiler: 'vue3' })],
   publicDir: false,
 });
